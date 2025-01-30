f33bf33cd2e55386b44ab5fd40cd32d373fe6b52
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-01-30 23:56:37 +0600

    feat: update user dashboard home page with a welcome message and improved styling

--- src/app/(dashboardLayout)/(userDashboard)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/(userDashboard)/dashboard/page.tsx
@@ -1,5 +1,11 @@
 const DashboardPage = () => {
-  return <div>User Dashboard Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center my-4">
+        Welcome to User Dashboard Home Page
+      </h1>
+    </div>
+  );
 };
 
 export default DashboardPage;
